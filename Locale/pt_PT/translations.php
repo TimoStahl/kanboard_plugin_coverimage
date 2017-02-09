0598c318e5ce58cd912ba305436cf5adea60314f
António Pereira <toni_to_1993@hotmail.com>
2017-02-09 17:57:44 +0000

    Updated pt_PT/translations.php (#15)

--- Locale/pt_PT/translations.php
+++ Locale/pt_PT/translations.php
@@ -1,14 +1,14 @@
 <?php
 
 return array(
-    //'Coverimage' => '',
-    //'Uploaded: %s' => '',
-    //'Size: %s' => '',
-    //'Uploaded by %s' => '',
-    //'Download' => '',
-    //'Remove' => '',
-    //'set as coverimage' => '',
-    //'remove coverimage' => '',
-    //'Coverimage set.' => '',
-    //'Coverimage removed.' => '',
+    'Coverimage' => 'Imagem de capa',
+    'Uploaded: %s' => 'Carregado a: %s',
+    'Size: %s' => 'Tamanho: %s',
+    'Uploaded by %s' => 'Carregado por %s',
+    'Download' => 'Download',
+    'Remove' => 'Remover',
+    'set as coverimage' => 'definir como imagem de capa',
+    'remove coverimage' => 'remover imagem de capa',
+    'Coverimage set.' => 'Imagem de capa definida.',
+    'Coverimage removed.' => 'Imagem de capa removida.',
 );
