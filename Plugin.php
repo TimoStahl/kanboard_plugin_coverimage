750f3f0de97627f00dbee3d8899cd35145700dbc
Timo Litzbarski <tili2@gmx.de>
2017-02-12 10:38:06 +0000

    add support for latest kanboard #16

--- Plugin.php
+++ Plugin.php
@@ -42,7 +42,7 @@ class Plugin extends Base {
     }
 
     public function getPluginVersion() {
-        return '1.0.31.0';
+        return '1.0.38.0';
     }
 
     public function getPluginHomepage() {
