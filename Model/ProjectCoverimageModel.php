411be965fddfb148f27b5cde0dc285d115d50e9f
Craig Crosby <creecros@gmail.com>
2018-10-04 14:13:54 -0400

    Update ProjectCoverimageModel.php

--- Model/ProjectCoverimageModel.php
+++ Model/ProjectCoverimageModel.php
@@ -8,17 +8,17 @@ class ProjectCoverimageModel extends ProjectFileModel {
 
     public function setCoverimage($project_id, $image_id) {
 
-        $this->projectMetadataModel->save($project_id, array('coverimage' => $image_id));
+        $this->projectMetadataModel->save($project_id, array('prjcoverimage' => $image_id));
     }
 
     public function removeCoverimage($task_id) {
 
-        $this->projectMetadataModel->remove($project_id, 'coverimage');
+        $this->projectMetadataModel->remove($project_id, 'prjcoverimage');
     }
 
     public function getCoverimage($task_id) {
 
-        $id = $this->projectMetadataModel->get($project_id, 'coverimage');
+        $id = $this->projectMetadataModel->get($project_id, 'prjcoverimage');
         if (!$id)
           return(null);
         return $this->getById($id);
