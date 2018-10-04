87b6bf9f8688d2ae47572ceb01da06c85fcb069b
Craig Crosby <creecros@gmail.com>
2018-10-04 15:14:22 -0400

    Create project_title.php

---
+++ Template/project_list/project_title.php
@@ -0,0 +1,21 @@
+<div>
+    <?php
+       $file = $this->task->projectCoverimageModel->getCoverimage($project['id']);
+         if(isset($file)){
+       ?>
+       <span class="proj-cover-image">
+          <img src="<?= $this->url->href('FileViewerController', 'thumbnail', array('file_id' => $file['id'], 'project_id' => $project['id'], 'task_id' => $file['task_id'])) ?>" title="<?= $this->text->e($file['name']) ?>" alt="<?= $this->text->e($file['name']) ?>" height="25">    
+       </span>
+       <?php
+         }
+       ?>
+    <?php if ($this->user->hasProjectAccess('ProjectViewController', 'show', $project['id'])): ?>
+        <?= $this->render('project/dropdown', array('project' => $project)) ?>
+    <?php else: ?>
+        <strong><?= '#'.$project['id'] ?></strong>
+    <?php endif ?>
+
+    <span class="table-list-title <?= $project['is_active'] == 0 ? 'status-closed' : '' ?>">
+        <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
+    </span>
+</div>
