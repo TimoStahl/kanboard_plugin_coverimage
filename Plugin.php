0ea7a4d7395821547d1055adf2a69b4d987cfc3f
Craig Crosby <creecros@gmail.com>
2018-10-05 13:29:53 -0400

    Project Logos (#21)
    * Create ProjectCoverimageModel.php
    
    * Create ProjectCoverimageController.php
    
    * Create images.php
    
    * Create overview.php
    
    * Update Plugin.php
    
    * Update Plugin.php
    
    * Update overview.php
    
    * Update overview.php
    
    * Update ProjectCoverimageModel.php
    
    * Update ProjectCoverimageModel.php
    
    * Update Plugin.php
    
    * Update Plugin.php
    
    * Update Plugin.php
    
    * Update Plugin.php
    
    * Update overview.php
    
    * Update images.php
    
    * Update overview.php
    
    * Update overview.php
    
    * Update overview.php
    
    * Update overview.php
    
    * Create project_title.php
    
    * Update Plugin.php
    
    * Update project_title.php
    
    * Update overview.php
    
    * Update README.md
    
    * Update ProjectCoverimageController.php
    
    * Update overview.php
    
    * Update project_title.php
    
    * Update overview.php
    
    * Update overview.php
    
    * Update project_title.php
    
    * Update images.php
    
    * Update images.php

--- Plugin.php
+++ Plugin.php
@@ -10,6 +10,9 @@ class Plugin extends Base {
     public function initialize() {
         //Task Details
         $this->template->setTemplateOverride('task_file/images', 'coverimage:task_file/images');
+        $this->template->setTemplateOverride('project_overview/images', 'coverimage:project_overview/images');
+        $this->template->setTemplateOverride('dashboard/overview', 'coverimage:dashboard/overview');
+        $this->template->setTemplateOverride('project_list/project_title', 'coverimage:project_list/project_title');
 
         //Board
         $this->hook->on('template:layout:css', array('template' => 'plugins/Coverimage/assets/css/board.css'));
@@ -25,6 +28,7 @@ class Plugin extends Base {
         return array(
             'Plugin\Coverimage\Model' => array(
                 'CoverimageModel',
+                'ProjectCoverimageModel',
             )
         );
     }
