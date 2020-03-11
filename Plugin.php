c6037d84d672173fdf18975f5497452eed405a4e
BlueTeck <tili2@gmx.de>
2020-03-11 13:57:23 +0100

    [INTERNAL] basic tests

--- Plugin.php
+++ Plugin.php
@@ -5,26 +5,34 @@ namespace Kanboard\Plugin\Coverimage;
 use Kanboard\Core\Plugin\Base;
 use Kanboard\Core\Translator;
 
-class Plugin extends Base {
+class Plugin extends Base
+{
 
-    public function initialize() {
-        //Task Details
-        $this->template->setTemplateOverride('task_file/images', 'coverimage:task_file/images');
-        $this->template->setTemplateOverride('project_overview/images', 'coverimage:project_overview/images');
-        $this->template->setTemplateOverride('dashboard/overview', 'coverimage:dashboard/overview');
-        $this->template->setTemplateOverride('project_list/project_title', 'coverimage:project_list/project_title');
-
-        //Board
+    public function initialize()
+    {
+        //General style
         $this->hook->on('template:layout:css', array('template' => 'plugins/Coverimage/assets/css/board.css'));
+
+        //Task
+        $this->template->setTemplateOverride('task_file/images', 'coverimage:task_file/images');
         $this->template->hook->attach('template:board:private:task:after-title', 'coverimage:board/task');
+
+        //Dashboard
+        $this->template->setTemplateOverride('dashboard/overview', 'coverimage:dashboard/overview');
+
+        //Project
+        $this->template->setTemplateOverride('project_overview/images', 'coverimage:project_overview/images');
+        $this->template->setTemplateOverride('project_list/project_title', 'coverimage:project_list/project_title');
     }
 
-    public function onStartup() {
+    public function onStartup()
+    {
         // Translation
         Translator::load($this->languageModel->getCurrentLanguage(), __DIR__ . '/Locale');
     }
 
-    public function getClasses() {
+    public function getClasses()
+    {
         return array(
             'Plugin\Coverimage\Model' => array(
                 'CoverimageModel',
@@ -33,24 +41,33 @@ class Plugin extends Base {
         );
     }
 
-    public function getPluginName() {
+    public function getPluginName()
+    {
         return 'Coverimage';
     }
 
-    public function getPluginDescription() {
+    public function getPluginDescription()
+    {
         return t('Coverimage Function');
     }
 
-    public function getPluginAuthor() {
+    public function getPluginAuthor()
+    {
         return 'BlueTeck + creecros';
     }
 
-    public function getPluginVersion() {
+    public function getPluginVersion()
+    {
         return '1.2.5.0';
     }
 
-    public function getPluginHomepage() {
+    public function getPluginHomepage()
+    {
         return 'https://github.com/BlueTeck/kanboard_plugin_coverimage';
     }
 
+    public function getCompatibleVersion()
+    {
+        return '>=1.2.5';
+    }
 }
